cb6781a1b29f40b76ad1c628b370148a7123badc
Theunis Cilliers <ardinent@gmail.com>
2017-08-01 10:35:56 +0200

    Small login page Update
    - Bootstrap Grid on register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,24 @@
 @section('title', config('app.name').' - Login')
 
 @section('content')
-    <div class="box-body">
-        {{ Form::open([
-            'method' => 'POST',
-            'route' => ['auth.login']
-        ]) }}
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="box-body">
+					{{ Form::open([
+						'method' => 'POST',
+						'route' => ['auth.login']
+					]) }}
 
-        @include('partials.forms.auth-login')
+					@include('partials.forms.auth-login')
 
-        {{ Form::submit('Login', array('class' => 'btn')) }}
+					{{ Form::submit('Login', array('class' => 'btn')) }}
 
-        {{ Form::close() }}
-    </div>
+					{{ Form::close() }}
+				</div>
 
-    <a href="/terms">Terms</a>
+				<a href="/terms">Terms</a>
+			</div>
+		</div>
+	</div>
 @stop
