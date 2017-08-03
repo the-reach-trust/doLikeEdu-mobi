adc5007b76eef8e52900545639aa947a21fc90ae
Theunis Cilliers <ardinent@gmail.com>
2017-08-03 13:14:47 +0200

    Home Page
    - Start work on the home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,23 +3,31 @@
 @section('title', config('app.name').' - Home')
 
 @section('content')
-	<div class="container">
-		<div class="row">
-			<div class="{{ config( 'front.dfltBodyClass' ) }} ">
-				<div>
-					Hello {{ 'name_here' }}
-					<p>You're flying through the list!</p>
-					<div class="row">
-						<div class="col-md-4"> {{ '1' }} </div>
-						<div class="col-md-4"> {{ '1' }} </div>
-						<div class="col-md-4"> {{ '100' }} </div>
-					</div>
-					<div class="row">
-						<div class="col-md-4">QUIZ</div>
-						<div class="col-md-4">POINTS</div>
-						<div class="col-md-4">LEVEL</div>
+	<div id="idprogresspanel">
+		<div class="container">
+			<div class="row">
+				<div class="{{ config( 'front.dfltBodyClass' ) }}">
+					<div class="text-center">
+						<h1>Hello {{ 'name_here' }}</h1>
+						<p>You're flying through the list!</p>
+						<div class="row">
+							<div class="col-md-4"><h3>{{ '1' }}</h3></div>
+							<div class="col-md-4"><h3>{{ '1' }}</h3></div>
+							<div class="col-md-4"><h3>{{ '100' }}</h3></div>
+						</div>
+						<div class="row">
+							<div class="col-md-4"><h5>QUIZ</h5></div>
+							<div class="col-md-4"><h5>POINTS</h5></div>
+							<div class="col-md-4"><h5>LEVEL</h5></div>
+						</div>
 					</div>
 				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="{{ config( 'front.dfltBodyClass' ) }} ">				
 
 				<p>Here's a list of things you can do today to earn points</p>
 
@@ -32,7 +40,7 @@
 				<hr>
 
 				<!-- hardcodded for testing now -->
-				<div>
+				<div class="text-center">
 					Need some help or advice ? <br/>
 					<a href="{{ route('pages.page', 1) }}">Click here for <b>TalkTo</b>Edu</a> <br/>
 				</div>
