e8be3ae6dc4c35555798b745602e65c6839b1e6c
Chris <christophermmiller101@gmail.com>
2017-08-08 10:00:51 +0200

    Terms initial commit

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -4,14 +4,205 @@
 
 @section('content')
 
-	<div class="container">
-		<div class="row">
-			<div class="{{ config( 'front.dfltBodyClass' ) }} ">
+<div class="container">
+	<div class="row">
+		<div class="{{ config( 'front.dfltBodyClass' ) }} ">
+			<p>Terms &amp; Conditions</p>
+			<p>DO LIKE EDU TERMS OF USE</p>
+			<ol>
+				<li>
+					<p>Introduction</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>DoLikeEdu is brought to Namibian learners by the Government of Namibia: Ministry of Education, Arts and Culture and Unicef. The copyright and all associated T&amp;Cs must rest with MoEAC.</p>
+				</li>
+				<li>
+					<p>The Terms of Use bind you from the moment you register with DoLikeEdu.</p>
+				</li>
+			</ol>
+			<ol start="2">
+				<li>
+					<p>Product terms and conditions</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>In the event of a conflict between the Terms of Use and the terms and conditions of a third party that is involved in rendering the DoLikeEdu services to you, these terms of Use will prevail insofar as your relationship with us is concerned.</p>
+				</li>
+				<li>
+					<p>We sometimes refer to information that is available outside DoLikeEdu. It is your responsibility to familiarise yourself with external information.</p>
+				</li>
+			</ol>
+			<ol start="3">
+				<li>
+					<p>Content</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>The content on DoLikeEdu is designed to highlight and explain parts of the school curriculum. We do not claim that the content is accurate, complete or that it represents the entire curriculum. We will do our reasonable best to ensure the quality of the content but you remain responsible to compare our content with the material you receive from your school. The material given to you by your school remains at all times the original version that you must use when studying and preparing for your assessments.</p>
+				</li>
+				<li>
+					<p>We do not accept liability for the information or data on any third party website or service, even if we have included a hyperlink to such information or data on our own site.</p>
+				</li>
+			</ol>
+			<ol start="4">
+				<li>
+					<p>Points</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>You can earn points when you successfully participate in quizzes available on DoLikeEdu. The quizzes are designed to test your understanding of the content.</p>
+				</li>
+				<li>
+					<p>We reserve the right to alter, modify and update the terms associated with points on DoLikeEdu at any time.</p>
+				</li>
+			</ol>
+			<ol start="5">
+				<li>
+					<p>Password and Security</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>When you register with DoLikeEdu you will log in using your contact number and chosen password. By registering with DoLikeEdu you give us permission to obtain the information that you have given permission to.</p>
+				</li>
+				<li>
+					<p>It is your responsibility to keep your log in details confidential. DoLikeEdu may from time to time publish guidelines on how to protect your log in details and you it is your responsibility to follow the guidelines.</p>
+				</li>
+				<li>
+					<p>You must immediately notify us if your log in details have been compromised.</p>
+				</li>
+				<li>
+					<p>Never disclose your password to anyone, including to members of our staff.</p>
+				</li>
+				<li>
+					<p>Whenever you are logged into DoLikeEdu we will assume that it is you and we will execute the instructions given during such session.</p>
+				</li>
+				<li>
+					<p>Please note that information sent through unsecured telecommunications networks can be subject to monitoring (unlawful or not). This is beyond the control of DoLikeEdu.</p>
+				</li>
+			</ol>
+			<ol start="6">
+				<li>
+					<p>Privacy</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>temp</p>
+				</li>
+			</ol>
+			<ol start="7">
+				<li>
+					<p>Availability</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>DoLikeEdu may at times not be available for various reasons and we have the discretion to suspend the service.</p>
+				</li>
+				<li>
+					<p>If DoLikeEdu is not available for whatever reason, we shall not be liable for any losses you may suffer as a consequence.</p>
+				</li>
+			</ol>
+			<ol start="8">
+				<li>
+					<p>Intellectual Property Rights</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>Some of the content on DoLikeEdu has been provided to us by third parties. Depending on the circumstances, we or our licensors retain copyright and other intellectual property rights in all material, including logos and other graphics and multimedia works published on DoLikeEdu. You may only use the content for personal use and may not be used not for any commercial purposes.</p>
+				</li>
+				<li>
+					<p>The logos and trademarks shown on DoLikeEdu are our registered and unregistered trademarks or that of third parties.</p>
+				</li>
+				<li>
+					<p>Nothing on this Terms of Use should be construed as granting you any license or right to use any intellectual property rights without our prior written permission and/or that of third parties, as the case may be.</p>
+				</li>
+				<li>
+					<p>If you submit any content to DoLikeEdu, you grant us a transferable, perpetual, world wide and royalty free license to use the content in any manner whatsoever. If the intellectual property rights of any content submitted by yourself is owned by a third party, you undertake to notify us immediately. You indemnify us against any claims that such third party may bring against us for the use of third party content submitted to DoLikeEdu by yourself.</p>
+				</li>
+			</ol>
+			<ol start="9">
+				<li>
+					<p>Disclaimer</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>We do not give any warranties, guarantees and / or representations, whether implied or applicable by law, other than the warranties expressly given in the Terms of Use.</p>
+				</li>
+			</ol>
+			<ol start="10">
+				<li>
+					<p>Notices</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>We shall send notices, irrespective of the medium of communication, that may be required by law or by this agreement to any of the addresses you designate on your personal profile. If a notice has been sent electronically it will be deemed that you received it 24 hours after we have dispatched the notice.</p>
+				</li>
+			</ol>
+			<ol start="11">
+				<li>
+					<p>Liability</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>We shall not be held responsible for any injury, loss, expense or damage of any kind whatsoever suffered or incurred by you as a result of using DoLikeEdu or relying on any information or content contained on DoLikeEdu, including but not limited to any injury, loss or damage suffered as a result of:</p>
+					<ol>
+						<li>
+							<p>errors or discrepancies in the information provided;</p>
+						</li>
+						<li>
+							<p>any breakdown or failure of any equipment used to access DoLikeEdu; or</p>
+						</li>
+						<li>
+							<p>the unavailability of DoLikeEdu.</p>
+						</li>
+					</ol>
+				</li>
+				<li>
+					<p>We reserve the right to alter, modify, upgrade, update, suspend or withdraw DoLikeEdu or any part hereof at any time.</p>
+				</li>
+				<li>
+					<p>You are entirely responsible for all content that you access, upload, post, email or otherwise transmit via DoLikeEdu. We are not liable for loss of any content you transmit and you should keep a backup copy of all such content.</p>
+				</li>
+			</ol>
+			<ol start="12">
+				<li>
+					<p>General</p>
+				</li>
+			</ol>
+			<ol>
+				<li>
+					<p>DoLikeEdu may from time to time amend the Terms of Use and the amendment will bind you from the first time you access DoLikeEdu subsequent to such amendments.</p>
+				</li>
+				<li>
+					<p>Each phrase, sentence, paragraph and clause in Terms of Use is severable the one from the other, notwithstanding the manner in which they may be linked together or grouped grammatically and if in terms of any judgement or order any phrase, sentence, paragraph or clause, is found to be defective or unenforceable for any reason the remaining phrases, sentences, paragraphs and clauses, as the case may be, shall nevertheless be and continue to be of full force and effect.</p>
+				</li>
+				<li>
+					<p>A certificate signed by the administrator responsible for maintaining DoLikeEdu will be proof of the date of publication and content of the current version and all previous versions of the conditions.</p>
+				</li>
+				<li>
+					<p style="margin-bottom: 0.1in; line-height: 100%"><a id="_GoBack" name="_GoBack"></a> <font color="#333333"><font face="Helvetica, serif"><font size="2" style="font-size: 10pt">The <font face="Helvetica, serif"><font size="2" style="font-size: 10pt">laws of the Namibia</font></font> <font color="#333333"><font face="Helvetica, serif"><font size="2" style="font-size: 10pt">to the Terms of Use.</p>
+				</li>
+				<li>
+					<p>Any breach by you of the Terms of Use entitles us to suspend your access to DoLikeEdu.</p>
+				</li>
+				<li>
+					<p>The rule of interpretation that a written agreement shall be interpreted against the party responsible for the drafting or the preparation of the agreement shall not apply.</p>
+				</li>
+			</ol>
 			
-				<p>Terms</p>
-			
-			</div>
 		</div>
 	</div>
-	
+</div>
+
 @stop
