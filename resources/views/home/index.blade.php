fceb88bfbd398f55b536d2b33dca3a99f38cb3cc
Chris <christophermmiller101@gmail.com>
2017-08-10 10:19:37 +0200

    missing text from completing dailys

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,8 +8,12 @@
 			<div class="row">
 				<div class="{{ config( 'front.dfltBodyClass' ) }}">
 					<div class="text-center">
-						<h1>Hello {{ 'name_here' }}</h1>
-						<p>You're flying through the list!</p>
+						<h1>{{ $dailys_complete ? 'Well done, ' : 'Hello' }} {{ 'name_here' }}</h1>
+						@if($dailys_complete)
+							<p>You finished all of today's featured quizzes. Nice work!</p>
+						@else
+							<p>You're flying through the list!</p>
+						@endif
 						<div class="row">
 							<div class="col-md-4"><h3>{{ '1' }}</h3></div>
 							<div class="col-md-4"><h3>{{ '1' }}</h3></div>
@@ -29,7 +33,12 @@
 		<div class="row">
 			<div class="{{ config( 'front.dfltBodyClass' ) }} ">
 				@if($dailys_complete)
-
+					<p class="text-center">
+						<img src="https://via.placeholder.com/175x200"> <br/>
+						<b>You finished all of today's featured quizzes. Nice work!</b> <br/>
+						<a href="{{ route('quizzes.index') }}">Click here if you want to do a few more.</a>
+					</p>
+				@else
 					<p>Here's a list of things you can do today to earn points</p>
 
 					<div>
@@ -37,12 +46,6 @@
 						<a href="{{ route('profile.index') }}">Finish your profile</a> <br/>
 						<a href="{{ route('quizzes.index') }}">Finish 2 more quizzes</a> <br/>
 					</div>
-				@else
-					<p class="text-center">
-						<img src="https://via.placeholder.com/175x200"> <br/>
-						<b>You finished all of today's featured quizzes. Nice work!</b> <br/>
-						<a href="{{ route('quizzes.index') }}">Click here if you want to do a few more.</a>
-					</p>
 				@endif
 
 				<hr>
