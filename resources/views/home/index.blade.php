104c451dbcdad74f5893222f3aeffbe561122a6a
Theunis Cilliers <ardinent@gmail.com>
2017-08-14 14:37:43 +0200

    More theme work
    - Fixed one line where I overrode Chris' variable for display home page
    content
    - Added another style
    - Added some theme utility classes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,16 +5,16 @@
 @section('content')
 	@include( 'partials.progresspanel.max-md' )
 
-	<div class="container">
+	<div class="container page-container">
 		<div class="row">
 
 			@if( config( 'front.progressiveDesktop' ) ) @include( 'partials.progresspanel.lg' ) @endif
 			<div class="{{ get_body_class( Request::route(), true ) }}">
-				@if( false && $dailys_complete )
+				@if( $dailys_complete )
 					<div class="text-center">
 						<img src="images/icon-balloon.png" width="200"> <br/>
 						<p class="h2">You finished all of today's featured quizzes. Nice work!</p>
-						<a href="{{ route('quizzes.index') }}">Click here if you want to do a few more.</a>
+						<a href="{{ route('quizzes.index') }}" class="theme-primary">Click here if you want to do a few more.</a>
 					</div>
 				@else
 					<p class="text-center">Here's a list of things you can do today to earn points.</p>
@@ -49,7 +49,7 @@
 				<!-- hardcodded for testing now -->
 				<div class="text-center">
 					Need some help or advice ? <br/>
-					<a href="{{ route('pages.page', 1) }}">Click here for <b>TalkTo</b>Edu</a> <br/>
+					<a href="{{ route('pages.page', 1) }}" class="theme-body-text">Click here for <b>TalkTo</b>Edu</a> <br/>
 				</div>
 				<!-- Content call -->
 				<!--
