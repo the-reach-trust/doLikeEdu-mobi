104f025038a46fa5297181c39a2e4a1856ce119e
Chris <christophermmiller101@gmail.com>
2017-09-20 11:52:28 +0200

    fix img on progress page

--- resources/views/progress/list-progress.blade.php
+++ resources/views/progress/list-progress.blade.php
@@ -1,6 +1,6 @@
 @php $progress = ($category->completed/$category->amount)*100 @endphp
 <li>
-	<img src="/images/ic-{{ strtolower($category->name) }}.svg" alt="{{ $category->name }}">
+	<img src="/images/ic-{{ str_replace(' ', '_', strtolower($category->name)) }}.svg" alt="{{ $category->name }}">
 	<h2>
 		<a class="no-decorate theme-primary" title="" href="{{ route('quizzes.category', $category->category) }}">{{ $category->name }}</a>
 	</h2>
